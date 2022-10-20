f2d59f92f453c31796ef9b39a64a02c7c352b6c5
maany <imptodefeat@gmail.com>
2022-10-20 21:33:39 +0200

    flux pre-flight checks

--- scripts/cluster_config.py
+++ scripts/cluster_config.py
@@ -16,7 +16,9 @@ from pyrage import x25519
 from termcolor import colored
 
 
-AGE_PUBLIC_KEY_FILE = (pathlib.Path(__file__).parent.parent / "age.pubkey").resolve()
+AGE_PUBLIC_KEY_FILE = (pathlib.Path(
+    __file__).parent.parent / "age.pubkey").resolve()
+
 
 class ClusterType(Enum):
     """_summary_ The cluster type
@@ -48,15 +50,18 @@ def run(
         file=sys.stderr,
         flush=True,
     )
+    proc = None
     try:
         proc = subprocess.Popen(args, **kwargs)
         yield proc
     finally:
-        proc.terminate()
-        proc.kill()
+        if proc is not None:
+            proc.terminate()
+            proc.kill()
 
     if return_stdout:
         return proc.stdout
+
 
 class BaseConfiguration:
     def __init__(self, **kwargs) -> None:
@@ -73,11 +78,38 @@ class BaseConfiguration:
 
     def run(self):
         total_steps = len(self.steps)
-        print(colored(f"Running {self.__class__.__name__} with {total_steps} steps", "green", "on_yellow", attrs=["bold"]))
+        print(colored(f"Running {self.__class__.__name__} with {total_steps} steps",
+              "green", "on_yellow", attrs=["bold"]))
         for idx, step in enumerate(self.steps):
-            log_prefix = "[{}/{}]: {} : ".format(idx + 1, total_steps, step.__name__)
+            log_prefix = "[{}/{}]: {} : ".format(idx + 1,
+                                                 total_steps, step.__name__)
             print(colored(log_prefix + "Starting", "magenta", attrs=["bold"]))
             step(log_prefix, **self.kwargs)
+
+    def run_process(self, *cmd, log_prefix: str, handle_error=True):
+        completed_process = None
+        print(colored(f"{log_prefix} Running {' '.join(cmd[0])}", "yellow", attrs=["bold"]))
+        try:
+            completed_process = subprocess.run(
+                *cmd, env=os.environ.copy(), capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(colored(log_prefix, "Error running command: {}".format(
+                e), "red", attrs=["bold"]))
+            sys.exit(1)
+        return_code, out, err = completed_process.returncode, completed_process.stdout, completed_process.stderr
+        out, err = out.decode(), err.decode()
+        if handle_error and return_code != 0:
+            print(colored(log_prefix, "Error running command: {}".format(
+                completed_process), "red", attrs=["bold"]))
+            print(log_prefix, out)
+            print(log_prefix, colored(err, "red"))
+            print(log_prefix,return_code)
+            print(colored(log_prefix, "Exiting", "red", attrs=["bold"]))
+            sys.exit(1)
+        print(colored(f"{log_prefix} Command completed successfully", "green"))
+        print(f"{log_prefix} ", out)
+        print(colored(f"{log_prefix} {err}", "green"))
+        return return_code, out, err
 
 
 class ClusterConfiguration(BaseConfiguration):
@@ -88,31 +120,37 @@ class ClusterConfiguration(BaseConfiguration):
             self.create_flux_system_namespace,
             self.setup_age_secret,
             self.setup_github_secret,
-            
+
         ]
 
     def create_flux_system_namespace(self, log_prefix: str | None = None, **kwargs):
         all_namespaces = self.v1.list_namespace()
         if "flux-system" in [ns.metadata.name for ns in all_namespaces.items]:
-            self.log(log_prefix, colored("flux-system namespace already exists", "yellow"))
+            self.log(log_prefix, colored(
+                "flux-system namespace already exists", "yellow"))
             return
         self.log(log_prefix, "Creating flux-system namespace")
         self.v1.create_namespace(body={"metadata": {"name": "flux-system"}})
 
     def setup_age_secret(self, log_prefix: str | None = None, **kwargs):
         # get the current secrets
-        current_secrets = self.v1.list_namespaced_secret(namespace="flux-system")
+        current_secrets = self.v1.list_namespaced_secret(
+            namespace="flux-system")
         # check if the secret already exists
         if "sops-age" in [secret.metadata.name for secret in current_secrets.items]:
             if not kwargs.get("force_age_secret", False):
-                self.log(log_prefix, colored("Age secret already exists, skipping", "blue"))
+                self.log(log_prefix, colored(
+                    "Age secret already exists, skipping", "blue"))
                 return
             self.log(
                 log_prefix,
-                colored("Age secret already exists, but force_age_secret is set to True, recreating", "yellow"),
+                colored(
+                    "Age secret already exists, but force_age_secret is set to True, recreating", "yellow"),
             )
-            self.log(log_prefix, colored("Deleting existing age secret", "red"))
-            self.v1.delete_namespaced_secret(name="sops-age", namespace="flux-system")
+            self.log(log_prefix, colored(
+                "Deleting existing age secret", "red"))
+            self.v1.delete_namespaced_secret(
+                name="sops-age", namespace="flux-system")
         # create the secret
         self.age_id = x25519.Identity.generate()
         self.age_pubkey = str(self.age_id.to_public())
@@ -131,32 +169,42 @@ class ClusterConfiguration(BaseConfiguration):
         self.log(log_prefix, "Creating secret", json.dumps(secret, indent=4))
         self.v1.create_namespaced_secret(namespace="flux-system", body=secret)
         with open(AGE_PUBLIC_KEY_FILE, "w") as f:
-            self.log(log_prefix, f"Writing age public key to", colored(AGE_PUBLIC_KEY_FILE, "green", attrs=["bold"]))
+            self.log(log_prefix, f"Writing age public key to", colored(
+                AGE_PUBLIC_KEY_FILE, "green", attrs=["bold"]))
             f.write(self.age_pubkey)
 
     def setup_github_secret(self, log_prefix: str | None = None, **kwargs):
         # get the current secrets
-        current_secrets = self.v1.list_namespaced_secret(namespace="flux-system")
+        current_secrets = self.v1.list_namespaced_secret(
+            namespace="flux-system")
         # check if the secret already exists
         if "github-flux-auth" in [secret.metadata.name for secret in current_secrets.items]:
             if not kwargs.get("force_github_secret", False):
-                self.log(log_prefix, colored("Github secret already exists, skipping", "blue"))
+                self.log(log_prefix, colored(
+                    "Github secret already exists, skipping", "blue"))
                 return
             self.log(
                 log_prefix,
-                colored("Github secret already exists, but force_github_secret is set to True, recreating", "yellow"),
+                colored(
+                    "Github secret already exists, but force_github_secret is set to True, recreating", "yellow"),
             )
-            self.log(log_prefix, colored("Deleting existing github secret", "red"))
-            self.v1.delete_namespaced_secret(name="github-flux-auth", namespace="flux-system")
+            self.log(log_prefix, colored(
+                "Deleting existing github secret", "red"))
+            self.v1.delete_namespaced_secret(
+                name="github-flux-auth", namespace="flux-system")
         # create the secret
         gh_user = kwargs.get("gh_user")
         if not gh_user:
-            self.log(log_prefix, colored("No github user provided", "red", attrs=["bold", "blink"]))
-            raise ValueError("No github user provided. Please provide a github user using the --gh-user flag")
+            self.log(log_prefix, colored(
+                "No github user provided", "red", attrs=["bold", "blink"]))
+            raise ValueError(
+                "No github user provided. Please provide a github user using the --gh-user flag")
         gh_password = kwargs.get("gh_password")
         if not gh_password:
-            self.log(log_prefix, colored("No github password provided", "red", attrs=["bold", "blink"]))
-            raise ValueError("No github password provided. Please provide a github password using the --gh-password flag")
+            self.log(log_prefix, colored(
+                "No github password provided", "red", attrs=["bold", "blink"]))
+            raise ValueError(
+                "No github password provided. Please provide a github password using the --gh-password flag")
 
         secret = {
             "kind": "Secret",
@@ -173,30 +221,41 @@ class ClusterConfiguration(BaseConfiguration):
         self.log(log_prefix, "Creating secret", json.dumps(secret, indent=4))
         self.v1.create_namespaced_secret(namespace="flux-system", body=secret)
 
+
 class FluxConfiguration(BaseConfiguration):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.cluster_name = kwargs.get("cluster_name")
-        if not self.cluster_name:
-            raise ValueError("No cluster name provided. Please provide a cluster name using the --cluster-name flag")
-        
+        self.kubeconfig = kwargs.get("kube_config")
+        if self.kubeconfig is None:
+            self.log(self.__class__.__name__, colored(
+                "No kubeconfig provided", "red", attrs=["bold", "blink"]))
+            raise ValueError(
+                "No kubeconfig provided. Please provide a kubeconfig using the --kube-config flag")
+        self.env = {"KUBECONFIG": self.kubeconfig, "PATH": os.environ["PATH"]}
         self.steps = [
             self.flux_preflight_check,
             self.generate_flux_config,
             self.install_flux
         ]
+
     def flux_preflight_check(self, log_prefix: str | None = None, **kwargs):
-        run("flux check --pre", log_prefix=log_prefix)
-    
+        self.run_process(["flux", "check", "--pre"], log_prefix=log_prefix)
+        
+
     def generate_flux_config(self, log_prefix: str | None = None, **kwargs):
         pass
 
     def install_flux(self, log_prefix: str | None = None, **kwargs):
+        # if not self.cluster_name:
+        # raise ValueError("No cluster name provided. Please provide a cluster name using the --cluster-name flag")
         pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cluster configuration")
-    parser.add_argument("--kube-config", type=str, help="Path to kubeconfig file")
+    parser.add_argument("--kube-config", type=str,
+                        help="Path to kubeconfig file")
     parser.add_argument(
         "--force-age-secret",
         action="store_true",
@@ -252,6 +311,7 @@ if __name__ == "__main__":
             gh_user=args.gh_user,
             gh_password=args.gh_password,
             force_github_secret=args.force_github_secret,
+            kube_config=args.kube_config,
         )
         flux_config.run()
         # proc.wait()
